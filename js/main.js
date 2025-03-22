9bc1d5700840651434364ee4aa1473825588d723
Anica <anicagabriellips@gmail.com>
2025-03-22 13:24:55 -0300

    aula1

---
+++ js/main.js
@@ -0,0 +1,17 @@
+class Pessoa
+{
+    nome
+    idade
+    peso
+    altura
+}
+
+const pessoa1 = new Pessoa();
+
+pessoa1.nome = 'maria';
+pessoa1.idade = 17;
+pessoa1.peso = 56.43;
+pessoa1.altura = 1.56;
+
+console.log(pessoa1);
+
